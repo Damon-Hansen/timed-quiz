e273faea4b2e6a3e9e7f6c4bd5bf8f70f92e0b61
Damon-Hansen <95259338+Damon-Hansen@users.noreply.github.com>
2022-01-22 16:35:33 -0600

    refractored original code

--- assets/script.js
+++ assets/script.js
@@ -1,115 +1,111 @@
-var quizContainer = document.querySelector("#quiz");
-var resultsContainer = document.querySelector("#results");
-var submitButton = document.querySelector("#submit");
-var questions = [
+// var quizContainer = document.querySelector("#quiz");
+// var resultsContainer = document.querySelector("#results");
+// var submitButton = document.querySelector("#submit");
+var startButton = document.querySelector("#start-btn");
+var timerEl = document.querySelector("#timer-countdown");
+var questionOrder = document.querySelector("#quiz-container");
+var myQuestion = [
     {
         question: "Who wrote the Harry Potter book series?",
-        answers: {
-            a: "J. R. R. Tolkien",
-            b: "J.K. Rowling",
-            c: "George R.R. Martin",
-            d: "Rick Riordan"
-        },
-        correctAnswer: "b"
+        answers: ["J. R. R. Tolkien", "J.K. Rowling", "George R.R. Martin", "Rick Riordan"],
+        correctAnswer: "J.K. Rowling"
     },
     {
         question: "What is the original name of the first Harry Potter book?",
-        answers: {
-            a: "Harry Potter and the Philosopher's Stone",
-            b: "Harry Potter and the Chamber of Secrets",
-            c: "Harry Potter and the Sorcerer's Stone",
-            d: "Harry Potter and the fall of the Dark Lord"
-        },
-        correctAnswer: "a"
+        answers: ["Harry Potter and the Philosopher's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Sorcerer's Stone", "Harry Potter and the fall of the Dark Lord"],   
+        correctAnswer: "Harry Potter and the Philosopher's Stone"
     },
     {
         question: "What year of school does Harry learn the spell 'Sectum Sempra'?",
-        answers: {
-            a: "Year 2",
-            b: "Year 4",
-            c: "Year 6",
-            d: "Year 7"
-        },
-        correctAnswer: "c"
+        answers: ["Year 2", "Year 4", "Year 6", "Year 7"],
+        correctAnswer: "Year 6"
     },
     {
         question: "What was the nickname of James Potter?",
-        answers: {
-            a: "Padfoot",
-            b: "Prongs",
-            c: "Moony",
-            d: "Wormtail"
-        },
-        correctAnswer: "b"
+        answers: ["Padfoot", "Prongs", "Moony", "Wormtail"],      
+        correctAnswer: "Prongs"
     },
     {
         question: "Who gave Harry the Nimbus 2000?",
-        answers:{
-            a: "Professor McGonagall",
-            b: "Professor Dumbledore",
-            c: "Professor Flitwick",
-            d: "Professor Snape"
-        },
-        correctAnswer: "a"
+        answers:["Professor McGonagall", "Professor Dumbledore", "Professor Flitwick", "Professor Snape"],
+        correctAnswer: "Professor McGonagall"
     },
     {
         question: "Who killed Sirius Black?",
-        answers: {
-            a: "Lucius Malfoy",
-            b: "Bellatrix Lestrange",
-            c: "Lord Voldemort",
-            d: "Barty Crouch Jr."
-        },
-        correctAnswer: "b"
+        answers: ["Lucius Malfoy", "Bellatrix Lestrange", "Lord Voldemort", "Barty Crouch Jr."],
+        correctAnswer: "Bellatrix Lestrange"
     },
     {
         question: "Who killed Bellatrix Lestrange?",
-        answers: {
-            a: "Ron Weasley",
-            b: "Molly Weasley",
-            c: "Fred Weasley",
-            d: "Ginny Weasley"
-        },
-        correctAnswer: "b"
+        answers: ["Ron Weasley", "Molly Weasley", "Fred Weasley", "Ginny Weasley"],
+        correctAnswer: "Molly Weasley"
     },
     {
         question: "What was in the golden snitch that Dumbledore left for Harry?",
-        answers: {
-            a: "Resurrection Stone",
-            b: "A Ring",
-            c: "Gold",
-            d: "A note"
-        },
-        correctAnswer: "a"
+        answers: ["Resurrection Stone", "A Ring", "Gold", "A note"],
+        correctAnswer: "Resurrection Stone"
+
     },
     {
         question: "Who opened the Chamber of Secrets?",
-        answers: {
-            a: "Harry Potter",
-            b: "Draco Malfoy",
-            c: "Ginny Weasley",
-            d: "Lucius Malfoy"
-        },
-        correctAnswer: "c"
+        answers: ["Harry Potter", "Draco Malfoy", "Ginny Weasley", "Lucius Malfoy"],             
+        correctAnswer: "Ginny Weasley"
+       
     },
     {
         question: "What kind of dragon did Harry face in the first task of the Triwizard Tournament?",
-        answers: {
-            a: "Norwegian Ridgeback",
-            b: "Hungarian Horntail",
-            c: "Swedish Short-Snout",
-            d: "Chinese Fireball"
-        },
-        correctAnswer: "b"
-    },
-]
+        answers: ["Norwegian Ridgeback", "Hungarian Horntail", "Swedish Short-Snout", "Chinese Fireball"],
+        correctAnswer: "Hungarian Horntail"        
+    }
+];
 
-var buildQuiz = function() {
+var startQuiz = function() {
+    startButton.style.display = 'none'; 
+    countDown();
+    nextQuestion();
+}
 
+function countDown() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 0) {
+            timerEl.textContent = timeLeft;
+            timeLeft--
+    }
+    else {
+        timerEl.textContent = "";
+        clearInterval(timeInterval);
+        displayMessage();
+        }
+    }, 1000);
 };
 
-var showResults = function() {
+var questionIndex = -1;
+function nextQuestion() {
+    questionOrder.innerHTML = '';
+    ++questionIndex;
+    console.log(questionOrder);
+        questionOrder.innerHTML += myQuestion[questionIndex].question + "<br />";
 
+        for (var i = 0; i < myQuestion[questionIndex].answers.length; i++) {
+            questionOrder.innerHTML += "<input type=radio id=myRadio name=radAnswer>" + myQuestion[questionIndex].answers[i] + "<br />";
+        }
+
+        if (questionIndex < (myQuestion.length - 1)) {
+            var nextButton = document.createElement("input");
+            nextButton.type = "button";
+            nextButton.value = "Next";
+            nextButton.addEventListener('click', nextQuestion);
+            questionOrder.appendChild(nextButton);
+        }
 };
 
-submitButton.addEventListener("click", showResults);
+// var showResults = function() {
+
+// };
+
+// buildQuiz();
+
+startButton.addEventListener("click", startQuiz);
+// submitButton.addEventListener("click", showResults);
